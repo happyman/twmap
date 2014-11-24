46f8b1ba4ea0b44fcbbc70a1ce785bd1eb8a6d72
happyman <happyman.eric@gmail.com>
2014-11-24 19:38:53 +0800

    將 mysql 換成 postgresql

--- twmap_gen/browseMap.php
+++ twmap_gen/browseMap.php
@@ -2,15 +2,26 @@
 // 1x1 km tile cache browser
 // use with lib/cli_tile_regen.php
 
-$x=$_REQUEST['x'];
-$y=$_REQUEST['y'];
-$sx=$_REQUEST['sx'];
-$sy=$_REQUEST['sy'];
-$ph = $_REQUEST['ph'];
+if (php_sapi_name() == "cli") {
+	$x=$argv[1];
+	$y=$argv[2];
+	$sx=$argv[3];
+	$sy=$argv[4];
+	$ph=$argv[5];
+} else {
+	$x=$_REQUEST['x'];
+	$y=$_REQUEST['y'];
+	$sx=$_REQUEST['sx'];
+	$sy=$_REQUEST['sy'];
+	$ph = $_REQUEST['ph'];
 
-if ($_REQUEST['type'] == 'image') {
-	outimage($x,$y,$ph);
-	exit(0);
+	if ($_REQUEST['type'] == 'image') {
+		outimage($x,$y,$ph);
+		exit(0);
+	} else if ($_REQUEST['type'] == 'image256') {
+		outimage2($x,$y,$ph);
+		exit(0);
+	}
 }
 if ($x == 0 || $y == 0  || $sx == 0 || $sy == 0 ) {
 	echo "error params";
@@ -21,15 +32,16 @@ if ($x == 0 || $y == 0  || $sx == 0 || $sy == 0 ) {
 <style>
 td {
 	width: 315px;
-  height: 315px;
+	height: 315px;
 	background-size: 100%;
 }
 body, html {
-  padding: 0px;
-	  margin: 0px;
-		  border: 0px none;
+	padding: 0px;
+		margin: 0px;
+			border: 0px none;
 					}
 </style>
+	<script type="text/javascript" src="js/jquery-1.4.4.min.js"></script>
 <body>
 <?
 // time xvfb-run  -a -s "-screen 0 640x480x16" wkhtmltoimage  --format png 
@@ -43,17 +55,47 @@ echo "<table width=$tw height=$ty border=0 cellspacing=0 cellpadding=0>";
 for($j=$y;$j>$y-$sy;$j--) {
 	echo "<tr>";
 	for($i=$x;$i<$x+$sx;$i++) {
-		printf('<td style="background: url(\'?type=image&x=%d&y=%d&ph=%d\') center no-repeat; width: 315px; height: 315px;">%s</td>' . "\n",
-			$i,$j,$ph , "<span class='gridx'>$i</span>,<span class='gridy'>$j</span></td>");
+		printf('<td style="background: url(\'browseMap.php?type=image&x=%d&y=%d&ph=%d\') center no-repeat; width: 315px; height: 315px;">%s</td>' . "\n",
+			$i,$j,$ph , "<div class=reload><span class='gridx'>$i</span>,<span class='gridy'>$j</span></div></td>");
 		//
 	}
 }
-echo "</table></body></html>";
-
+echo "</table>";
+?>
+<script>
+$(".reload").click(function(){
+		var x =$(this).find(".gridx").text();
+		var y =$(this).find(".gridy").text();
+		//console.log($(this).find(".gridx").text());
+	//	alert(x + "," + y);
+		var url = "lib/cli_tile_regen.php?x="+x+"&y="+y+"&sx=1&sy=1";
+	console.log(url);
+		window.open(url, "_blank");
+});
+</script>
+</body></html>";
+<?php
 function outimage($x,$y,$ph) {
 	$path = sprintf("/mnt/twmapcache/cache/16/%s/%d/%d_%d.png",($ph==1)?"ph":"tw",$x,$x,$y);
-	if (file_exists($path))
-	header("Content-type: image/png");
-	header("X-file-location: $path");
-	readfile($path);
+	if (file_exists($path)) {
+		header("Content-type: image/png");
+		header("X-file-location: $path");
+		readfile($path);
+	} else {
+		header("Content-type: image/jpeg");
+		readfile("imgs/null.jpg");
+	}
+}
+function outimage2($ix,$iy,$ph=0) {
+	$x=$ix+150;
+	$y=2800-$iy;
+	$path = sprintf("/mnt/twmapcache/cache/16/%s/%d/%d_%d.png",($ph==1)?"ph":"tw",$x,$x,$y);
+	if (file_exists($path)) {
+		header("Content-type: image/png");
+		header("X-file-location: $path");
+		readfile($path);
+	} else {
+		header("Content-type: image/jpeg");
+		readfile("imgs/null.jpg");
+	}
 }
--- twmap_gen/lib/cli_tile_regen.php
+++ twmap_gen/lib/cli_tile_regen.php
@@ -2,16 +2,30 @@
 require_once "../config.inc.php";
 require_once  "tiles.inc.php";
 
-$x = isset($argv[1]) ? intval($argv[1]) : 0;
-$y = isset($argv[2]) ? intval($argv[2]) : 0;
-$sx = isset($argv[3]) ? intval($argv[3]) : 0;
-$sy = isset($argv[4]) ? intval($argv[4]) : 0;
-$debug = 1;
+if (php_sapi_name() == "cli") { 
+	$x = isset($argv[1]) ? intval($argv[1]) : 0;
+	$y = isset($argv[2]) ? intval($argv[2]) : 0;
+	$sx = isset($argv[3]) ? intval($argv[3]) : 0;
+	$sy = isset($argv[4]) ? intval($argv[4]) : 0;
+	$debug = 1;
+} else {
+	$x = $_GET['x'];
+	$y = $_GET['y'];
+	$sx = $_GET['sx'];
+	$sy = $_GET['sy'];
+	$debug = 1;
+}
 
 if ($x == 0 || $y == 0 ) {
 	echo "$argv[0] x y sx sy\n";
 	echo " regenerate v3 1x1 tile cache\n";
 	exit(0);
+}
+for ($i=0 ; $i<1000 ; $i++) {
+	    echo ".";
+			    ob_flush();
+			    flush();
+					//    sleep(1);
 }
 for ($i=$x;$i<$x+$sx;$i++){
 	for($j=$y;$j>$y-$sy;$j--) {
@@ -21,9 +35,10 @@ for ($i=$x;$i<$x+$sx;$i++){
 function warn($x,$y) {
 	global $tilepath;
 	global $debug; 
-	list ($ret, $img, $cached) = img_from_tiles($tilepath, $x, $y, 1, 1, 16, 0, $debug, "/dev/shm","/mnt/twmapcache/cache", 1);
+	// 2 force regen
+	list ($ret, $img, $cached) = img_from_tiles($tilepath, $x, $y, 1, 1, 16, 0, $debug, "/dev/shm","/mnt/twmapcache/cache", 2);
 	if ($ret === true ) {
-		@unlink($img);
+		//@unlink($img);
 		if ($cached == "cached") return;
 		echo "$x $y ok\n";
 	}
