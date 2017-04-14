e704db6006078da21aa6e05bf5ebc895f72d79e3
happyman <happyman.eric@gmail.com>
2017-04-14 21:20:42 +0800

    add kml presentation of prominence

--- docs/prominence/src/prom2kml.php
+++ docs/prominence/src/prom2kml.php
@@ -3,7 +3,7 @@
 // 2. column 4 名稱要有值　也就是最好有　peak name　
 // 3. head: sn,peak_x,peak_y,peak_h,peak_name,prominence,col_x,col_y,col_h,parent_x,parent_y,parent_h,parent_name
 $opt=getopt("i:");
-if (!isset($opt['i'])){　
+if (!isset($opt['i'])){
 	echo "Usage: $argv[0] -i prom.csv > prom.kml\n";
 	exit(0);
 	
@@ -44,6 +44,7 @@ printf("<name>台灣獨立峰列表 by 地表突起度</name>");
 <?php
 foreach($data as $line) {
 	$row = explode(",",trim($line));
+	if ($row[1]=='peak_x') continue;
 	if ($row[3] > 3000) {
 		// if (count($mt3000) > 100) continue;
 		if ($row[5] < 100) continue; 
@@ -56,32 +57,57 @@ foreach($data as $line) {
 		$mt2000[] = $row;
 	}
 }
-outfolder($mt3000,"高山");
-outfolder($mt2000,"中級山");
-outfolder($mt1500,"郊山");
+if (!empty($mt3000))
+	outfolder($mt3000,"高山");
+if (!empty($mt2000))
+	outfolder($mt2000,"中級山");
+if (!empty($mt1500))
+	outfolder($mt1500,"郊山");
+
 printf("</Document></kml>\n");
 function outpoint($row,$index){
-		printf("<Placemark>\n<name>%d(%d),%s [%d] %dM to %dM</name><styleUrl>#peak</styleUrl>\n",$index,$row[0],$row[4],$row[5],$row[3],$row[8]);
+		// only one folder
+		if ($index == $row[0])
+			printf("<Placemark>\n<name>%d %s (%d)</name><styleUrl>#peak</styleUrl>\n",$index,$row[4],$row[5]);
+		else
+			printf("<Placemark>\n<name>%d(%d),%s [%d] %dM to %dM</name><styleUrl>#peak</styleUrl>\n",$index,$row[0],$row[4],$row[5],$row[3],$row[8]);
+		
 		printf("<description><![CDATA[高度 %.0f<br>突起度=%.0f]]></description> \n", $row[3], $row[5]);
 	printf("   <Point>    <coordinates>%f,%f,%d</coordinates>  <altitudeMode>absolute</altitudeMode>\n", $row[1],$row[2],$row[3]);
 
 	printf("</Point>\n</Placemark>\n");
 	if (!empty($row[6])){
+		// peak to key col
 		printf("<Placemark><name>%s to %d</name>%s<LineString>",$row[4],$row[8],linewidth($row[5]));
 		printf(" <coordinates>%f,%f,%d\n", $row[1],$row[2],$row[3]);
 		printf(" %f,%f,%d\n", $row[6],$row[7],$row[8]);
 		printf(" </coordinates>\n<altitudeMode>absolute</altitudeMode>\n");
 		printf("</LineString>\n</Placemark>");
-		// coll
-		$key=sprintf("%f_%f_%d",$row[6],$row[7],$row[8]);
+		// key col to parent peak
+		printf("<Placemark><name>%s to %d</name>%s<LineString>",$row[12],$row[8],linewidth($row[5]));
+		printf(" <coordinates>%f,%f,%d\n", $row[9],$row[10],$row[11]);
+		printf(" %f,%f,%d\n", $row[6],$row[7],$row[8]);
+		printf(" </coordinates>\n<altitudeMode>absolute</altitudeMode>\n");
+		printf("</LineString>\n</Placemark>");
+		$key=sprintf("%f_%f_%d_1",$row[6],$row[7],$row[8]);
 		if (!isset($icon[$key])){
-			printf("<Placemark><name>col: %s</name><styleUrl>#col</styleUrl><Style><IconStyle><heading>%f</heading></IconStyle></Style>\n",$row[8],headings($row[1],$row[2],$row[6],$row[7]));
+			printf("<Placemark><name>%s 主鞍</name><styleUrl>#col2</styleUrl><Style><IconStyle><heading>%f</heading></IconStyle></Style>\n",$row[4],headings($row[1],$row[2],$row[6],$row[7]));
 				printf("<description><![CDATA[%s %dM 的主鞍部=%.0f]]></description> \n", $row[4], $row[3], $row[8]);
 			printf("    <Point>   <coordinates>%f,%f,%d</coordinates>\n<altitudeMode>absolute</altitudeMode>\n", $row[6],$row[7],$row[8]);
 		
 			printf("</Point>\n</Placemark>\n");
 			$icon[$key] = 1;
+		}		
+		$key=sprintf("%f_%f_%d_2",$row[6],$row[7],$row[8]);
+		if (!isset($icon[$key])){
+			printf("<Placemark><name>%s M to %s M</name><styleUrl>#col</styleUrl><Style><IconStyle><heading>%f</heading></IconStyle></Style>\n",$row[8],$row[11],headings($row[9],$row[10],$row[6],$row[7]));
+				printf("<description><![CDATA[%s %dM 的主鞍部=%.0f 指向父峰 %s %dM]]></description> \n", $row[4], $row[3], $row[8], $row[12],$row[11]);
+			printf("    <Point>   <coordinates>%f,%f,%d</coordinates>\n<altitudeMode>absolute</altitudeMode>\n", $row[6],$row[7],$row[8]);
+		
+			printf("</Point>\n</Placemark>\n");
+			$icon[$key] = 1;
 		}
+		
 	}
 }
 function linewidth($z){
@@ -121,6 +147,9 @@ function outfolder($data,$name){
 	</Style> ");
 	printf("<Style id=\"col\">
 	 <IconStyle> <Icon> <href>http://maps.google.com/mapfiles/kml/pal4/icon28.png</href> </Icon></IconStyle>
+	</Style> ");
+		printf("<Style id=\"col2\">
+	 <IconStyle> <Icon> <href>https://www.google.com/mapfiles/arrow.png</href> </Icon></IconStyle>
 	</Style> ");
 	for($i=0;$i<count($data);$i++){
 		outpoint($data[$i],$i+1);
