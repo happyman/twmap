e930d344736f43b53003518523ce181b3c09c25b
happyman <happyman.eric@gmail.com>
2023-06-25 07:15:58 +0800

    check before run

--- twmap_gen/worker/make_map/checkmakemap.sh
+++ twmap_gen/worker/make_map/checkmakemap.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 cd  /home/happyman/make_map
-for i in $(seq 1 2);
+for i in $(seq 1 10);
 do
 ps ax |grep make_map0$i |grep -v grep|grep -v tmux > /dev/null
 if [ $? -ne 0 ]; then
-	echo "create session m$i"
-	tmux new-session -d -s m$i php make_map0$i.php &
+	if [ -f "make_map0$i.php" ]; then
+		echo "create session m$i"
+		tmux new-session -d -s m$i php make_map0$i.php &
+	fi
 fi
 done
