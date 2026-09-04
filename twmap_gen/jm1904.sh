31030524c235da9dd3823c3fe8426771ffed4080
happyman <happyman.eric@gmail.com>
2026-09-04 15:03:29 +0800

    remove test stuff

--- twmap_gen/jm1904.sh
+++
@@ -1,8 +0,0 @@
-rm /home/happyman/map/out/test/259000x2730*
-debug=-d
-# 1. normal
-#php cmd_make2.php -r 243:2658:3:3:TWD97 -O ~/map/out/test -v 1921 -t test -D 3x4 -m /dev/shm $debug
-# 2. with GPX
-php cmd_make2.php -r 259:2730:3:3:TWD97 -O ~/map/out/test -v 1904 -G -t test -D 3x4 -D 5x7 -m /dev/shm $debug
-
-# -G merge
--- twmap_gen/jm1924.sh
+++
@@ -1,8 +0,0 @@
-rm /home/happyman/map/out/test/244000x*
-debug=-d
-# 1. normal
-#php cmd_make2.php -r 243:2658:3:3:TWD97 -O ~/map/out/test -v 1921 -t test -D 3x4 -m /dev/shm $debug
-# 2. with GPX
-php cmd_make2.php -r 244:2511:3:3:TWD97 -O ~/map/out/test -v 1924 -G -t test -D 3x4 -D 5x7 -m /dev/shm $debug
-
-# -G merge
