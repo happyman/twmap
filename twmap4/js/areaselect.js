8a51f914166f0554aa803d4e103502003bc54b2b
happyman <happyman.eric@gmail.com>
2026-09-07 13:33:32 +0800

    fix open url in _top

--- twmap4/js/areaselect.js
+++ twmap4/js/areaselect.js
@@ -179,7 +179,8 @@ function AreaSelect(opts) {
     var target = url + callmake + '&title=' + encodeURIComponent(title) + '&datum=' + datum;
     closeModal();
     if (window.confirm('程式將會傳送參數給地圖產生器,確定嘛?')) {
-      window.top.location.href = target;
+       // window.top.location.href = target;
+	window.open(target, '_top');
     }
   }
 
